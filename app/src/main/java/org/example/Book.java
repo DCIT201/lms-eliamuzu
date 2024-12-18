a3bda99039a5531e633f2786d847efc976470d8a
Sunset30 <kofiprince375@gmail.com>
2024-12-18 22:33:03 +0000

    book class updated

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,42 @@
-public class Book {
+package org.example;
 
-    // This is your working class.
+public class Book {
+    // Initializing Private fields
+    private String Title;
+    private String Author;
+    private int YearPublished;
+
+    //Book Constructor 
+    public Book(String Title, String Author, int YearPublished){
+        this.Title = Title;
+        this.Author = Author;
+        this.YearPublished = YearPublished;
+    }
+
+    //Getters and Setters for fields
+    public String getTitle(){
+        return Title;
+    }
+    
+    public void setTitle(String Title){
+        this.Title = Title;
+    }
+
+    public String getAuthor(){
+        return Author;
+    }
+
+    public void setAuthor(String Author){
+        this.Author = Author;
+    }
+
+    public int getYearPublished(){
+        return YearPublished;
+    }
+
+    public void setYearPublished(int YearPublished){
+        this.YearPublished = YearPublished;
+    }
     
 }
+
